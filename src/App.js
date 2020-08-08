7c481f239f563ed00d89e254ea170b238341f437
john1eng <john1eng@hotmail.com>
2020-08-08 18:54:54 -0400

    second commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import HelloComponent from './Hello';
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <HelloComponent />
     </div>
   );
 }
---
+++ src/Hello.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const HelloComponent = () => {
+
+    return (
+        <h1>
+            Welcome, Hello!
+        </h1>
+    );
+
+}
+
+export default HelloComponent;
